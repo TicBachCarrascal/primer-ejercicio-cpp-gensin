5fe8accca9a93fe3bb1e11d6ad1ba7cd14eefc4c
Pau <ghostdesert@gmail.com>
2022-02-17 11:02:50 +0100

    Ejercicios

--- mates.cpp
+++ mates.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main(){
-	std::cout << 6+3;
-	std::cout << 48%5;
+	std::cout <<6+7;
+	std::cout <<48%5;
+	std::cout <<"6+7=" << 6+7;
+	std::cout <<"6+7=" <<std::endl << 6+7 <<std::endl ;
 	return 0;
 }
