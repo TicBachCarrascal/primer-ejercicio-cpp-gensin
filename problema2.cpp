9d009bbf69abd7036eb627212ba336669c72afbd
Pau <ghostdesert@gmail.com>
2022-03-02 10:49:32 +0100

    Problema 2 cutre

---
+++ problema2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+	float PI = 3.14159265;
+	float grados = 30;
+	double gradosRadianes = grados*PI/180;
+	cout << "El coseno de " << grados << " es " << cos(gradosRadianes) << endl;
+	grados = 45;
+	gradosRadianes = grados*PI/180;
+	cout << "El coseno de " << grados << " es " << cos(gradosRadianes) << endl;	 		 
+	grados = 60;
+	gradosRadianes = grados*PI/180;
+	cout << "El coseno de " << grados << " es " << cos(gradosRadianes) << endl;	 		 
+	grados = 90;
+	gradosRadianes = grados*PI/180;
+	cout << "El coseno de " << grados << " es " << cos(gradosRadianes) << endl;	 		 
+	return 0;
+}
