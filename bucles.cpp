a15917bfc7b2b36616f83b83a89d098205071ac4
Pau <ghostdesert@gmail.com>
2022-03-24 10:22:00 +0100

    bucle for

--- bucles.cpp
+++ bucles.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main(){
-	int numero, contador = 0;
-
-	for(numero = 1; numero <=10; numero=numero+1){
+	
+	for(int numero=1;numero<=10;numero+=2){
 		cout << numero << " ";
 	}
 	return 0;
